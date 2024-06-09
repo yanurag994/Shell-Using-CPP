29072b45f1cc14f9ae958385574b6205abff6302
Anurag Yadav <yanurag994@gmail.com>
2024-06-08 21:52:47 -0400

    Reading from input

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@ int main() {
   std::cerr << std::unitbuf;
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
+  //std::cout << "Logs from your program will appear here!\n";
 
   // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-  //
-  // std::string input;
-  // std::getline(std::cin, input);
+  std::cout << "$ ";
+  
+  std::string input;
+  std::getline(std::cin, input);
 }
